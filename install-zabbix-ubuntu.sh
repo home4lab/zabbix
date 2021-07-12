019a8e3810ebd8cfd36dd9ece3e843d6aeb5f020
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-12 09:47:55 +0700

    Create install-zabbix-ubuntu.sh

---
+++ install-zabbix-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu20.04_all.deb
+dpkg -i zabbix-release_5.4-1+ubuntu20.04_all.deb
+apt update
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql
+
+
+
+mysql -uroot -p
+mysql --defaults-extra-file=sql.cnf -e "create database zabbix character set utf8 collate utf8_bin;"
+mysql --defaults-extra-file=sql.cnf -e "create user zabbix@localhost identified by 'zabbix';"
+mysql --defaults-extra-file=sql.cnf -e "grant all privileges on zabbix.* to zabbix@localhost;"
+
