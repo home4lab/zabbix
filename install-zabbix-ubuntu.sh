902974d163f2f91d91af16db3e0d386dc879f039
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-12 11:50:11 +0700

    Update install-zabbix-ubuntu.sh

--- install-zabbix-ubuntu.sh
+++ install-zabbix-ubuntu.sh
@@ -2,29 +2,43 @@
 DB_USER="zabbix"
 DB_PASS="zabbix"
 
+echo ""
+echo " === DOWNLOAD PACKAGE FROM ZABBIX REPOSITORY === "
+echo ""
 
-#echo "download zabbix.deb"
-#wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu20.04_all.deb
-#echo "installing zabbix.deb"
-#dpkg -i zabbix-release_5.4-1+ubuntu20.04_all.deb
-#echo "update and install"
-#apt update
-#apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-server -y
+wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu20.04_all.deb
 
-echo "login and create database zabbix"
+echo ""
+echo " === INSTALLING === "
+echo ""
 
-#mysql
+dpkg -i zabbix-release_5.4-1+ubuntu20.04_all.deb
+
+echo ""
+echo " === INSTALL ZABBIX WITH MYSQL === "
+echo ""
+
+apt update
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-server -y
+
+echo ""
+echo " === CREATE ZABBIX DATABASE === "
+echo ""
+
 mysql -e "create database zabbix character set utf8 collate utf8_bin;"
 mysql -e "create user zabbix@localhost identified by 'zabbix';"
 mysql -e "grant all privileges on zabbix.* to zabbix@localhost;"
 
-#create database zabbix character set utf8 collate utf8_bin;
-#create user zabbix@localhost identified by 'zabbix';
-#grant all privileges on zabbix.* to zabbix@localhost;
-
-echo "import schema sql"
+echo ""
+echo " === IMPORT SCHEMA SQL === "
+echo ""
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/create.sql.gz | mysql -uzabbix -p${DB_PASS} zabbix
 
 systemctl restart zabbix-server zabbix-agent apache2
 systemctl enable zabbix-server zabbix-agent apache2
+
+
+echo ""
+echo " OPEN BROWSER http://your_ip/zabbix "
+echo ""
 
