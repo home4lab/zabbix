f3c9b4c301737578f7ff588220a690af93d77ab7
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-12 11:52:40 +0700

    Update install-zabbix-ubuntu.sh

--- install-zabbix-ubuntu.sh
+++ install-zabbix-ubuntu.sh
@@ -5,26 +5,22 @@ DB_PASS="zabbix"
 echo ""
 echo " === DOWNLOAD PACKAGE FROM ZABBIX REPOSITORY === "
 echo ""
-
 wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu20.04_all.deb
 
 echo ""
 echo " === INSTALLING === "
 echo ""
-
 dpkg -i zabbix-release_5.4-1+ubuntu20.04_all.deb
 
 echo ""
 echo " === INSTALL ZABBIX WITH MYSQL === "
 echo ""
-
 apt update
 apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-server -y
 
 echo ""
 echo " === CREATE ZABBIX DATABASE === "
 echo ""
-
 mysql -e "create database zabbix character set utf8 collate utf8_bin;"
 mysql -e "create user zabbix@localhost identified by 'zabbix';"
 mysql -e "grant all privileges on zabbix.* to zabbix@localhost;"
@@ -34,8 +30,17 @@ echo " === IMPORT SCHEMA SQL === "
 echo ""
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/create.sql.gz | mysql -uzabbix -p${DB_PASS} zabbix
 
+echo ""
+echo " === ENABLE SERVICE ZABBIX SERVER AGENT AND APACHE === "
+echo ""
 systemctl restart zabbix-server zabbix-agent apache2
 systemctl enable zabbix-server zabbix-agent apache2
+
+
+echo ""
+echo " === COPYING ZABBIX SERVER CONF === "
+echo ""
+cp zabbix_server.conf /usr/share/zabbix/conf/
 
 
 echo ""
