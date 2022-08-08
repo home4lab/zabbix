225d984c3ef35a5416af222d542fb639e8040ed0
home4lab <87222176+home4lab@users.noreply.github.com>
2022-08-08 10:36:38 +0700

    Update install-zabbix-postgresql.sh

--- install-zabbix-postgresql.sh
+++ install-zabbix-postgresql.sh
@@ -12,6 +12,13 @@ echo " === INSTALLING === "
 echo ""
 dpkg -i zabbix-release_6.2-1+ubuntu20.04_all.deb
 
+
+echo ""
+echo " === UPDATE REPO POSTGRESQL === "
+echo ""
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
 echo ""
 echo " === INSTALL ZABBIX WITH MYSQL === "
 echo ""
